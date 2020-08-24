3b3cedc4557cacf61b86b65693004274169e7ca7
chemistryy <70154608+chemistryy@users.noreply.github.com>
2020-08-24 20:49:28 +0500

    Delete rightCol.php

--- mysite/www/blocks/rightCol.php
+++
@@ -1,5 +0,0 @@
-<div id="rightCol">
-		<div class="banner"><img src="/img/banner_1.jpg" alt="Место для вашей рекламы" title="Место для вашей рекламы"></div>
-		<div class="banner"><img src="/img/banner_2.jpg" alt="Место для вашей рекламы" title="Место для вашей рекламы"></div>
-		<div class="banner"><img src="/img/banners.jpg" alt="Место для вашей рекламы" title="Место для вашей рекламы"></div>
-</div>
