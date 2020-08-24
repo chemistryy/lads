cd5e5cd280be8ace65f6b44a514d5566b1ad8939
chemistryy <70154608+chemistryy@users.noreply.github.com>
2020-08-24 20:31:16 +0500

    new site

---
+++ blocks/footer.php
@@ -0,0 +1,14 @@
+<footer>
+  	<div id="social">
+  	  <ol>
+  	  	<li><a href="http://facebook.com" target="_blank"><i class="fa fa-facebook" aria hidden="true"></i></a></li>
+  	  	<li><a href="" target="_blank"><i class="fa fa-twitter" aria hidden="true"></i></a></li>
+  	  	<li><a href="" target="_blank"><i class="fa fa-google-plus" aria hidden="true"></i></a></li>
+  	  	<li><a href="" target="_blank"><i class="fa fa-telegram" aria hidden="true"></i></a></li>
+  	  	<li><a href="" target="_blank"><i class="fa fa-instagram" aria hidden="true"></i></a></li>
+  	  </ol>
+  	</div>
+  	<div id="rights">
+  		Все права защищены &copy; <?php echo date ('Y')?>
+  	</div>
+</footer>
